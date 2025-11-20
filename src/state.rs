d95a4673604e57c24e6bc9643da15794a1bcaca4
tland357 <42850249+tland357@users.noreply.github.com>
2025-11-19 20:40:33 -0500

    default effect is set

--- src/state.rs
+++ src/state.rs
@@ -14,9 +14,13 @@ pub struct AppState {
 }
 
 pub fn create_app_state(dev : Device) -> AppState {
+    let mut mp = HashMap::new();
+    let v: Value = Value::from("RB Jams");
+    mp.insert("effect".to_string(), v);
     let state = AppState {
-        store: Arc::new(RwLock::new(HashMap::new())),
+        store: Arc::new(RwLock::new(mp)),
         device: dev,
     };
+
     return state;
 }
